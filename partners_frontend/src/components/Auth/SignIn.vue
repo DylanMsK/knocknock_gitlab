7858e4a4a21d95cb801295e7b754a559812eb41f
HAN_DONG_HUN <pok_winter@naver.com>
2019-10-16 12:23:26 +0900

    signin 페이지 UI 구성

--- partners_frontend/src/components/Auth/SignIn.vue
+++ partners_frontend/src/components/Auth/SignIn.vue
@@ -1,5 +1,32 @@
 <template>
-  
+  <v-row>
+    <v-col cols="12">
+      <p class="login-font mb-0">낰낰</p>
+    </v-col>
+    <v-col cols="12">
+      <v-text-field
+        class="Noto-Sans-KR"
+        label="이메일"
+        outlined
+        rounded
+        clearable
+      ></v-text-field>
+      <v-text-field
+        class="Noto-Sans-KR"
+        label="비밀번호"
+        type="password"
+        outlined
+        rounded
+        clearable
+      ></v-text-field>
+    </v-col>
+    <v-col class="center pt-0" cols="12">
+      <v-btn class="Noto-Sans-KR" x-large color="primary">로그인 하기</v-btn>
+    </v-col>
+    <v-col class="Noto-Sans-KR center bottom-position fill-width">
+      낰낰의 계정이 없으신가요?<a class="bold-signup-font px-3">회원가입</a>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -8,6 +35,24 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style>
+.login-font {
+  font-family: 'Noto Sans KR', sans-serif;
+  font-size: 72px;
+  font-weight: 900;
+}
+.Noto-Sans-KR {
+  font-family: 'Noto Sans KR', sans-serif;
+}
+.center {
+  display: flex;
+  justify-content: center;
+}
+.bottom-position {
+  position: absolute;
+  bottom: 12px;
+}
+.bold-signup-font {
+  font-weight: 600;
+}
 </style>
--- partners_frontend/src/pages/Auth.vue
+++ partners_frontend/src/pages/Auth.vue
@@ -1,13 +1,23 @@
 <template>
-  
+  <v-container fill-height>
+    <SignIn/>
+  </v-container>
 </template>
 
 <script>
+import SignIn from '../components/Auth/SignIn'
+import SignUp from '../components/Auth/SignUp'
+
 export default {
-  
+  components: {
+    SignIn,
+    SignUp
+  }
 }
 </script>
 
 <style scoped>
-
+.Noto-Sans-KR {
+  font-family: 'Noto Sans KR', sans-serif;
+}
 </style>
