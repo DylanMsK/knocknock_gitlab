32154e9c5e035aa532036e25c0888f071724aab5
HyehuiLee <lhh9577@daum.net>
2019-10-16 11:44:04 +0900

    add font & change backgroundcolor

--- clients_frontend/src/App.vue
+++ clients_frontend/src/App.vue
@@ -1,36 +1,16 @@
 <template>
-  <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+  <v-app class="white-bg">
+    <v-container class="nanum-gothic-coding">
+      <router-view />
+    </v-container>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data: () => ({
-    //
-  })
+<style scoped>
+.white-bg {
+  background-color: white !important;
 }
-</script>
+.nanum-gothic-coding {
+  font-family: 'Nanum Gothic Coding', monospace !important;
+}
+</style>
