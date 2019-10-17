9b0eb676530ab8241d8b0eb5952bab7c095b8335
HyehuiLee <lhh9577@daum.net>
2019-10-17 18:37:00 +0900

    Kakao 지도 api 추가

--- clients_frontend/src/pages/MainPage.vue
+++ clients_frontend/src/pages/MainPage.vue
@@ -1,31 +1,12 @@
 <template>
-  <v-container>
-    <div
-      id="map"
-      class="map-300h"
-    >
-    </div>
-
-  </v-container>
+  <Map />
 </template>
 
 <script>
+import Map from '../components/Main/Map'
 export default {
-  mounted () {
-    var container = document.getElementById('map')
-    var options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3
-    }
-
-    var map = new kakao.maps.Map(container, options)
+  components: {
+    Map
   }
 }
 </script>
-
-<style scoped>
-.map-300h {
-  width: 100%;
-  height: 300px;
-}
-</style>
