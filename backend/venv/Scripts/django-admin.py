a35f2235745cad44ca4ef75bf3b93ae5b57523c4
HAN_DONG_HUN <pok_winter@naver.com>
2019-10-17 10:30:45 +0900

    remove venv & restframework install

--- backend/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\multicampus\Documents\GitHub\s1p1151009\backend\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
