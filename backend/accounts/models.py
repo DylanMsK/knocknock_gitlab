6f0009815b882702804551284f2535e34ce6648d
강민수 <kms920612@gmail.com>
2019-10-18 13:22:53 +0900

    client & partner 테스트 모델 생성

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return "★[사용자] {} | {}".format(self.id, self.nickname)
+
+
+class Partner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return "☆[파트너] {} | {}".format(self.id)
+        
