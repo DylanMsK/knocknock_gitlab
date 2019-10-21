3d3a93b4073a2716813be880b64afcc0d1c72a19
강민수 <kms920612@gmail.com>
2019-10-21 15:46:50 +0900

    admin site에 Option 모델 추가

--- backend/stores/admin.py
+++ backend/stores/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from stores.models import Category, Store
+from stores.models import Category, Option, Store
 
 # Register your models here.
 admin.site.register(Category)
+admin.site.register(Option)
 admin.site.register(Store)
