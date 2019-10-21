1cbc10161a73627ea944c441d3670fe7951117a5
HAN_DONG_HUN <pok_winter@naver.com>
2019-10-21 15:40:00 +0900

    indexing 에러 수정

--- partners_frontend/src/store/modules/postText.js
+++ partners_frontend/src/store/modules/postText.js
@@ -1,6 +1,6 @@
 const state = {
   textBox: false,
-  postContent: '아프리카 돼지 열병 사태로 당분간 영업을 하지 않습니다. 혹시나 국밥이 마려우신 분은 성진네 국밥가게를 가주시기 바랍니다.'
+  postContent: '아프리카 돼지 열병 사태로 당분간 영업을 하지 않습니다. 혹시나 국밥이 마려우신 분은 성진이네 국밥가게를 가주시기 바랍니다.'
 }
 
 const mutations = {
