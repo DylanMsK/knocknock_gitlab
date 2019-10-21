44037eda6941568f1c01d6feedeaa77313fe514b
HAN_DONG_HUN <pok_winter@naver.com>
2019-10-21 17:41:20 +0900

    register Page 제작

--- partners_frontend/src/pages/RegisterStorePage.vue
+++ partners_frontend/src/pages/RegisterStorePage.vue
@@ -2,19 +2,122 @@
   <v-col>
     <div cols="12" class="register-head mb-4">영업장 등록</div>
     <div>
-      <p class="register-title">상호 명</p>
       <v-text-field
-        class="text-area-font"
-      >
-      </v-text-field>
+        label="사업자 성명"
+        v-model="partnerName"
+        :error-messages="partnerNameErrors"
+        @input="$v.partnerName.$touch()"
+        @blur="$v.partnerName.$touch()"
+        required
+        outlined
+        rounded
+        clearable
+        autocomplete="off"
+      ></v-text-field>
+      <v-text-field
+        label="상호명"
+        v-model="storeName"
+        :error-messages="storeNameErrors"
+        @input="$v.storeName.$touch()"
+        @blur="$v.storeName.$touch()"
+        required
+        outlined
+        rounded
+        clearable
+        autocomplete="off"
+      ></v-text-field>
+      <v-text-field
+        label="사업장 주소"
+        v-model="address"
+        :error-messages="addressErrors"
+        @input="$v.address.$touch()"
+        @blur="$v.address.$touch()"
+        required
+        outlined
+        rounded
+        clearable
+        autocomplete="off"
+      ></v-text-field>
+      <v-text-field
+        label="사업 등록번호"
+        v-model="regiNumber"
+        :error-messages="regiNumberErrors"
+        @input="$v.regiNumber.$touch()"
+        @blur="$v.regiNumber.$touch()"
+        required
+        outlined
+        rounded
+        clearable
+        autocomplete="off"
+      ></v-text-field>
+      <v-file-input class="mt-0" label="사업자 등록증을 첨부하세요."></v-file-input>
     </div>
     상호, 성명, 사업장 주소, 등록번호, 캡처
   </v-col>
 </template>
 
 <script>
+import { validationMixin } from 'vuelidate'
+import { required, email, minLength, sameAs } from 'vuelidate/lib/validators'
+
 export default {
-  
+  data () {
+    return {
+      storeName: '',
+      partnerName: '',
+      address: '',
+      regiNumber: ''
+    }
+  },
+  mixins: [validationMixin],
+  validations: {
+    storeName: {
+      required
+    },
+    partnerName: {
+      required
+    },
+    address: {
+      required
+    },
+    regiNumber: {
+      required
+    }
+  },
+  computed: {
+    storeNameErrors () {
+      const errors = []
+      if (!this.$v.storeName.$dirty) {
+        return errors
+      };
+      !this.$v.storeName.required && errors.push('상호명을 입력해주세요.')
+      return errors
+    },
+    partnerNameErrors () {
+      const errors = []
+      if (!this.$v.partnerName.$dirty) {
+        return errors
+      };
+      !this.$v.partnerName.required && errors.push('사업자 성명을 입력해주세요.')
+      return errors
+    },
+    addressErrors () {
+      const errors = []
+      if (!this.$v.address.$dirty) {
+        return errors
+      };
+      !this.$v.address.required && errors.push('사업장 주소를 입력해주세요.')
+      return errors
+    },
+    regiNumberErrors () {
+      const errors = []
+      if (!this.$v.regiNumber.$dirty) {
+        return errors
+      };
+      !this.$v.regiNumber.required && errors.push('사업 등록번호를 입력해주세요.')
+      return errors
+    }
+  }
 }
 </script>
 
