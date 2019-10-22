5d44096390d46644fa836c4d8d6fc7ed6db5eca6
강민수 <kms920612@gmail.com>
2019-10-22 12:04:33 +0900

    client, partner 회원가입 로그인 구현

--- backend/KNOCKKNOCK/urls.py
+++ backend/KNOCKKNOCK/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/stores/', include('stores.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('stores/', include('stores.urls')),
 ]
 
 if settings.DEBUG:
