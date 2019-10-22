816882da44f6520c8abec6d466519a8d01190643
강민수 <kms920612@gmail.com>
2019-10-22 12:03:00 +0900

    partner 모델에 필드 추가 및 related_name 지정 및 마이그레이션

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class Client(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='client')
     nickname = models.CharField(max_length=20)
     
     def __str__(self):
@@ -11,13 +11,10 @@ class Client(models.Model):
 
 
 class Partner(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=30)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='partner')
     phone = models.CharField(max_length=15)
-    email = models.CharField(max_length=50)
-    certificate = models.ImageField(upload_to='partners/certificates')
-    certificate_number = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+
 
     def __str__(self):
-        return f'☆[파트너] {self.id} | {self.name}'
-        
+        return f'☆[파트너] {self.id}'
