646958f29143739ce62d844bd51c03ec1b0ff0c5
강민수 <kms920612@gmail.com>
2019-10-23 11:32:56 +0900

    sqlite3로 db변경 및 allowed_host 추가

--- backend/KNOCKKNOCK/settings.py
+++ backend/KNOCKKNOCK/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '7#x6^)o8=alh5_7g!8z(n*m=_j)_o&do!a%k$50c%y6xpuzr1-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'KNOCKKNOCK.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        #  'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-         'ENGINE': 'django.contrib.gis.db.backends.postgis',
-         'NAME': 'postgres',
-         'USER': '',
-         'PASSWORD': '',
-         'HOST': 'localhost',
-         'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #  'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        #  'NAME': 'postgres',
+        #  'USER': '',
+        #  'PASSWORD': '',
+        #  'HOST': 'localhost',
+        #  'PORT': '5432',
     },
 }
 
