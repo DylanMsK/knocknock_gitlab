344c5561db1d5aea03bd935e21a86b4e9d3d4378
강민수 <kms920612@gmail.com>
2019-10-23 19:27:33 +0900

    postgres db 적용

--- backend/KNOCKKNOCK/settings.py
+++ backend/KNOCKKNOCK/settings.py
@@ -78,14 +78,14 @@ WSGI_APPLICATION = 'KNOCKKNOCK.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        #  'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        #  'NAME': 'postgres',
-        #  'USER': '',
-        #  'PASSWORD': '',
-        #  'HOST': 'localhost',
-        #  'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+         'NAME': 'test',
+         'USER': '',
+         'PASSWORD': '',
+         'HOST': 'localhost',
+         'PORT': '5432',
     },
 }
 
