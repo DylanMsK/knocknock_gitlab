706cda9fa3c3b5e54b5a2cce6ecf3be6c711b87e
HAN_DONG_HUN <pok_winter@naver.com>
2019-10-24 14:07:39 +0900

    백 url 수정 / 토큰이 만료될 케이스 수정

--- partners_frontend/src/main.js
+++ partners_frontend/src/main.js
@@ -11,10 +11,17 @@ new Vue({
   async created () {
     if ('user' in localStorage) {
       await store.dispatch('auth/userAuth')
-    } else {
+      if (this.$store.state.auth.error) { // 토큰을 가지고 있지만 이상한 토큰일 때
+        store.commit('auth/initError')
+        localStorage.removeItem('user')
+        router.push('/').catch(error => {
+          console.log('이동하려는 위치가 현재와 동일합니다. / ' + error.message)
+        })
+      }
+    } else { // 그냥 토큰이 없을 때
       if (this.$router.currentRoute.name !== 'authPage') {
-        router.push('/').catch(err => {
-          console.log('이동하려는 위치가 현재와 동일합니다. / ' + err.message)
+        router.push('/').catch(error => {
+          console.log('이동하려는 위치가 현재와 동일합니다. / ' + error.message)
         })
       }
     }
