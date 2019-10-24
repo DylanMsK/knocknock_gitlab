cdaf274f14d75c0972489a00ed5797537698790c
HyehuiLee <lhh9577@daum.net>
2019-10-24 17:27:39 +0900

    Map component 주소보여주기 & 새로고침 & appkey 숨김

--- clients_frontend/src/components/Main/Map.vue
+++ clients_frontend/src/components/Main/Map.vue
@@ -9,7 +9,13 @@
 			@load="onLoad"
 			@dblclick="zoomIn"
 			@click="onMapEvent($event)"
+			@dragend="getAddr"
+			@zoom_changed="getAddr"
+			@bounds_changed="getAddr"
 			class="map" />
+		<div class="hAddr">
+			<p class="ma-0">{{ this.centerAddr }}</p>
+		</div>
 		<div class="zoomControl radius_border">
 			<v-btn 
 				@click="zoomIn"
@@ -25,11 +31,20 @@
 				<v-icon>mdi-minus</v-icon>
 			</v-btn>
 		</div>
+		<div class="refreshBtn radius_border">
+			<v-btn
+				@click="currentAddr"
+				icon
+			>
+				<v-icon>mdi-refresh</v-icon>
+			</v-btn>
+		</div>
 	</div>
 </template>
 
 <script>
 import VueDaumMap from 'vue-daum-map'
+import config from '../../../map.config';
 
 export default {
 	components: {
@@ -37,14 +52,15 @@ export default {
 	},
 	data() {
 		return {
-			appKey: '74d1e73195cf5ac2a0bb5a6b0aa60434',
+			appKey: config.appKey,
 			center: {lat: 37.553190, lng: 126.972759}, 
 			level: 3,
 			mapTypeId: VueDaumMap.MapTypeId.NORMAL,
-			libraries: [],
+			libraries: ['services'],
 			map: null,
 			curMarker: null,
 			pointMarker: null,
+			centerAddr: '',
 		}
 	},
 	methods: {
@@ -59,8 +75,7 @@ export default {
 					this.center.lat = lat
 					this.center.lng = lng
 
-					// 현재 위치 마커 표시. 
-					// 현재 위치와 클릭한 곳 마커 이미지 어떻게 구분할지 못정해서 따로 빼놓는데, 구분 가능하면 아래코드 지우고 displayMarker 호출
+					// 현재 위치 마커 표시
 					this.map = map					
 					var imageSrc = 'http://testrottenwifi.ito.lt/bundles/itowififront/images/my-location-icon.png',
 							imageSize = new kakao.maps.Size(70, 70),
@@ -71,42 +86,99 @@ export default {
 						position: new kakao.maps.LatLng(lat, lng),
 						image: curMarkerImage
 					})
-					this.pointMarker = new kakao.maps.Marker({
-						map: map,
-						position: ''
-					})
 				})
 			} else {
 				console.log('geolocation 사용 불가능')
 			}
+			// 클릭한 곳 마커 표시
+			this.pointMarker = new kakao.maps.Marker({
+				map: map,
+				position: ''
+			})
+			this.searchAddrFromCoords(map.getCenter(), this.displayCenterInfo);
 		},
 		onMapEvent (params) {
 			this.pointMarker.setPosition(params[0].latLng)
+			this.searchAddrFromCoords(params[0].latLng, this.displayCenterInfo);
+		},
+		getAddr () {
+			this.searchAddrFromCoords(this.map.getCenter(), this.displayCenterInfo);
+		},
+		searchAddrFromCoords (coords, callback) {
+			var geocoder = new kakao.maps.services.Geocoder();
+			geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
+		},
+		displayCenterInfo (result, status) {
+    if (status === kakao.maps.services.Status.OK) {
+			var infoDiv = document.getElementById('centerAddr');
+			for(var i = 0; i < result.length; i++) {
+            // 행정동의 region_type 값은 'H' 이므로
+					if (result[i].region_type === 'H') {
+							this.centerAddr = result[i].address_name;
+							break;
+			}}}
+		},
+		currentAddr () {
+			if (navigator.geolocation) {
+					navigator.geolocation.getCurrentPosition((position) => {
+						var lat = position.coords.latitude,
+								lng = position.coords.longitude
+
+						// 지도 중심위치 바꿔줌
+						this.center.lat = lat
+						this.center.lng = lng
+
+						var coords = new kakao.maps.LatLng(lat, lng);
+						this.map.setCenter(coords);
+						this.level = 3
+
+						this.curMarker.setPosition(coords)
+			})}
 		},
 		zoomIn () {
 			if (this.level > 1) {
 				this.level -= 1
-			}
-		},
+		}
+	},
 		zoomOut () {
 			if (this.level < 14) {
 				this.level += 1
-			}
-		}
+		}}
 	}
 }
 </script>
 
 <style scoped>
+.map-wrap {
+	width: 100vw;
+	height: 50vh;
+}
 .map {
 	width: 100vw;
 	height: 50vh;
+}
+.hAddr {
+	position: absolute;
+	background-color: rgba(255, 255, 255, 0.8);
+	top: 105px;
+	left: 10px;
+	padding: 10px 10px;
+	z-index: 1;
 }
 .zoomControl {
 	position: absolute;
 	width: 40px;
 	background-color: white;
 	top: 200px;
+	right: 10px;
+	z-index: 1;
+	text-align: center;
+}
+.refreshBtn {
+	position: absolute;
+	width: 40px;
+	background-color: white;
+	top: 450px;
 	right: 10px;
 	z-index: 1;
 	text-align: center;
