ca9043326471821bc2484495379f0258fde86754
강민수 <kms920612@gmail.com>
2019-10-28 14:33:08 +0900

    ready to deploy

--- backend/KNOCKKNOCK/settings.py
+++ backend/KNOCKKNOCK/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7#x6^)o8=alh5_7g!8z(n*m=_j)_o&do!a%k$50c%y6xpuzr1-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
