528cd4f997dbe43bb45c6e938da0993b4e1250ff
강민수 <kms920612@gmail.com>
2019-10-28 13:58:18 +0900

    store 검색 api 와 거리별 필터링 api 분리

--- backend/stores/serializers.py
+++ backend/stores/serializers.py
@@ -18,7 +18,26 @@ class OptionSerializer(serializers.ModelSerializer):
 
 class StoreSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
-    options = OptionSerializer()
+    options = serializers.StringRelatedField(many=True)
+    distance = serializers.SerializerMethodField('get_distance')
+    addr = serializers.SerializerMethodField('get_old_address')
+
+    class Meta:
+        model = Store
+        fields = ('id', 'name', 'category', 'description', 'lon', 'lat', 'thumbnail', 'contact',
+                  'road_addr', 'addr', 'tags', 'price_avg', 'partner', 'review_cnt', 'view_cnt',
+                  'options', 'distance')
+
+    def get_distance(self, obj):
+        return int(obj.distance.m)
+
+    def get_old_address(self, obj):
+        return f'{obj.common_addr} {obj.addr}'
+
+
+class StoreSearchSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    options = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Store
