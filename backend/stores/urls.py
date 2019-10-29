766093603ce3ec4bcf62d11244d5e5bb93116bfb
강민수 <kms920612@gmail.com>
2019-10-29 17:40:01 +0900

    StoreSerializer 이름을 StoreByDistanceSerializer로 변경 & partnerFeedback 을 clientReviews 안에 넣음

--- backend/stores/urls.py
+++ backend/stores/urls.py
@@ -11,7 +11,7 @@ from stores.views import (
 
 urlpatterns = [
     path('', StoreListAPI.as_view(), name='store_list'),
-    path('<int:pk>/', StoreDetailAPI.as_view(), name='store_detail'),
+    path('<int:store_id>/', StoreDetailAPI.as_view(), name='store_detail'),
     path('<int:store_id>/reviews/', RetrieveCreateClientReviewAPI.as_view(), name='create_client_review'),
     path('<int:store_id>/reviews/<int:review_id>/', DeleteClientReivewAPI.as_view(), name='delete_client_review'),
     path('<int:store_id>/reviews/<int:review_id>/feedback/', RetrieveCreatePartnerFeedbackAPI.as_view(), name='create_partner_feedback'),
